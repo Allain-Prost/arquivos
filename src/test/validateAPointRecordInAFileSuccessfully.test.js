77220661d31ff4c434805f3ffd5a7a42006c49de
Allain-Prost <allain.prost@dcx.ufpb.br>
2023-09-09 00:54:27 -0300

    Fix: Alterando o caso de teste para realizar a busca da linha a busca binária

--- src/test/validateAPointRecordInAFileSuccessfully.test.js
+++ src/test/validateAPointRecordInAFileSuccessfully.test.js
@@ -1,19 +1,21 @@
-const { findAndSplitLine, readFileAndSeparateByLine } = require('../utils/funcitonFiles'),
-  { searchFileData } = require('../data/searchFile.data'),
+const { expectedMessagesTC001 } = require('../data/messages/auxiliaryMessages'),
+  { findAndSplitLine, readFileAndSeparateByLine } = require('../utils/functionFiles'),
+  { searchFileData, dataTC001 } = require('../data/searchFile.data'),
   { assert } = require('chai')
 
-describe('Teste em arquivos de ponto', () => {
-  it(`Deve encontrar e validar uma batida de ponto no arquivo ${searchFileData.fileName} com sucesso`, () => {
+describe('Teste em arquivos de ponto sem a busca binária', () => {
+  it(`[TC-002] - Deve validar um registro de ponto no arquivo ${searchFileData.fileName} com sucesso`, () => {
     const listOfLines = readFileAndSeparateByLine(searchFileData.filePath, searchFileData.encoding, '\n')
     const line = findAndSplitLine(searchFileData.lineToSearch, listOfLines, '|', searchFileData.fileName)
 
-    assert.equal(line[0], '05', 'O valor da entrada é diferente do esperado!')
-    assert.equal(line[1], '3208690', 'O XX é diferente do esperado!')
-    assert.equal(line[2], '2023-03-20T08:01:38-0300', 'A data é diferente do esperado!')
-    assert.equal(line[3], '100000001', 'O YYY é diferente do esperado!')
-    assert.equal(line[4], 'E', 'O valor do tipo da entrada é diferente do esperado!')
-    assert.equal(line[5], '001', 'O valor tal é diferente do esperado!')
-    assert.equal(line[6], 'O', 'O valor 6 é diferente do esperado!')
-    assert.equal(line[7], 'PADRÃO COMERCIAL', 'O valor do padrão é diferente do esperado!')
+    assert.equal(line[0], dataTC001.inputTypeNumber, expectedMessagesTC001.inputTypeNumber)
+    assert.equal(line[1], dataTC001.valueXX, expectedMessagesTC001.valueXX)
+    assert.equal(line[2], dataTC001.inputDate, expectedMessagesTC001.inputDate)
+    assert.equal(line[3], dataTC001.valueYYY, expectedMessagesTC001.valueYYY)
+    assert.equal(line[4], dataTC001.inputType, expectedMessagesTC001.inputType)
+    assert.equal(line[5], dataTC001.valueTal, expectedMessagesTC001.valueTal)
+    assert.equal(line[6], dataTC001.value6, expectedMessagesTC001.value6)
+    assert.equal(line[7], dataTC001.standardValue, expectedMessagesTC001.standardValue)
+    assert.equal(line[8] ?? null, dataTC001.pointInsertion, expectedMessagesTC001.pointInsertion)
   })
 })
