3d06acea481be84df8c70b4f6b47d2b9a3ab4538
Allain-Prost <allain.prost@dcx.ufpb.br>
2023-09-09 00:52:14 -0300

    Add massa de dados para o TC001

--- src/data/searchFile.data.js
+++ src/data/searchFile.data.js
@@ -2,11 +2,24 @@ const path = require('path');
 
 const searchFileData = {
   fileName: 'AEJ_VAN_ASS_V8H51.txt',
-  lineToSearch: '05|3208690|2023-03-20T08:01:38-0300|100000001|E|001|O|PADRÃO COMERCIAL|',
+  lineToSearch: '05|3208690|2023-03-17T13:30:00-0300|100000001|E|003|I|PADRÃO COMERCIAL|Ponto inserido manualmente',
   filePath: path.join(__dirname, '../arquivos/AEJ_VAN_ASS_V8H51.txt'),
   encoding: 'utf-8',
-}
+},
+
+  dataTC001 = {
+    inputTypeNumber: '05',
+    valueXX: '3208690',
+    inputDate: '2023-03-17T13:30:00-0300',
+    valueYYY: '100000001',
+    inputType: 'E',
+    valueTal: '003',
+    value6: 'I',
+    standardValue: 'PADRÃO COMERCIAL',
+    pointInsertion: 'Ponto inserido manualmente'
+  }
 
 module.exports = {
-  searchFileData
+  searchFileData,
+  dataTC001
 }
