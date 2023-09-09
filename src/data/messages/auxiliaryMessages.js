b0c574d2813a1ff3dfcc61cbf061704a95b16902
Allain-Prost <allain.prost@dcx.ufpb.br>
2023-09-09 00:53:09 -0300

    Add: massa de dados para menssagens quando houver algum erro

---
+++ src/data/messages/auxiliaryMessages.js
@@ -0,0 +1,15 @@
+const expectedMessagesTC001 = {
+  inputTypeNumber: 'O valor da entrada é diferente do esperado!',
+  valueXX: 'O XX é diferente do esperado!',
+  inputDate: 'A data é diferente do esperado!',
+  valueYYY: 'O YYY é diferente do esperado!',
+  inputType: 'O valor do tipo da entrada é diferente do esperado!',
+  valueTal: 'O valor tal é diferente do esperado!',
+  value6: 'O valor 6 é diferente do esperado!',
+  standardValue: 'O valor do padrão é diferente do esperado!',
+  pointInsertion: 'O valor do ponto é diferente do esperado!'
+}
+
+module.exports = {
+  expectedMessagesTC001
+}
